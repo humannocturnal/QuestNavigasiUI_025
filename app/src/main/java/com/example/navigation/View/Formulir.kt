00f746c791d663c991a5fda45e0cad9cf13b5010
humannocturnal <syaputrar374@gmail.com>
2025-11-07 10:19:26 +0700

    tambahkan input form untuk nama, jenis kelamin, dan alamat

--- app/src/main/java/com/example/navigation/View/Formulir.kt
+++ app/src/main/java/com/example/navigation/View/Formulir.kt
@@ -65,8 +65,20 @@ fun FormIsian(
                     }
                 }
             }
-
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = {Text(text="Alamat")},
+                onValueChange = {},
+            )
         }
-
     }
 }
