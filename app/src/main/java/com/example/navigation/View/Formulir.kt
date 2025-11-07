76cb05322ad1e1d89869b2ffadb61e49311d7d1d
humannocturnal <syaputrar374@gmail.com>
2025-11-07 09:50:13 +0700

    menambahkan komponen FormIsian dengan tombol submit

---
+++ app/src/main/java/com/example/navigation/View/Formulir.kt
@@ -0,0 +1,13 @@
+package com.example.navigation.View
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("laki-laki", "perempuan"),
+    onSubmitBTNClick: () -> Unit
+) {
+
+}
