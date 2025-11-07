788a2306d27b5383159b64f58e34c39003e4ddab
humannocturnal <syaputrar374@gmail.com>
2025-11-07 19:47:24 +0700

    menambahkan import untuk penyesuaian kode

--- app/src/main/java/com/example/navigation/Navigasi.kt
+++ app/src/main/java/com/example/navigation/Navigasi.kt
@@ -1,15 +1,15 @@
 package com.example.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.navigation.View.FormIsian
 import com.example.navigation.View.TampilData
-import java.lang.reflect.Modifier
-import java.nio.file.WatchEvent
 
 enum class Navigasi {
     Formulirku,
