9ba19ccc9053e93887bb8b4be9a38eeefce5dd1e
humannocturnal <syaputrar374@gmail.com>
2025-11-07 10:08:58 +0700

    tambahkan input OutlinedTextField untuk nama lengkap pada tampilan form

--- app/src/main/java/com/example/navigation/View/Formulir.kt
+++ app/src/main/java/com/example/navigation/View/Formulir.kt
@@ -1,17 +1,24 @@
 package com.example.navigation.View
 
 import android.R.attr.text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.R
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,5 +33,20 @@ fun FormIsian(
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = colorResource(id=R.color.teal.700))
             )}
-    ){}
+    ){isiRuang ->
+        Column (modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text="nama Lengkap")},
+                onValueChange = {},
+            )
+        }
+
+    }
 }
