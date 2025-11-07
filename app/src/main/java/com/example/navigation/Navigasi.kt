ee6ca69609ce5225f16788dc9cc98dda2a574afe
humannocturnal <syaputrar374@gmail.com>
2025-11-07 19:33:41 +0700

    menambahkan struktur navigasi utama dengan NavHost dan NavController

--- app/src/main/java/com/example/navigation/Navigasi.kt
+++ app/src/main/java/com/example/navigation/Navigasi.kt
@@ -1,6 +1,30 @@
 package com.example.navigation
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
+import java.nio.file.WatchEvent
+
 enum class Navigasi {
     Formulirku,
     Detail
+}
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+
+    Scaffold { isiRuang ->
+
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+
+        }
+    }
 }
